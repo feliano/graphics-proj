a4bd1bc8299649767beaebcd17735c35d431ee7f
Fredrik <felia@kth.se>
2017-09-15 10:56:49 +0200

    frees memory for cube textures

--- Cube.cpp
+++ Cube.cpp
@@ -232,6 +232,9 @@ void Cube::loadCubeTexture(){
                 GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 
                 0, GL_RGBA, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
         }
+
+        stbi_image_free(image);
+
     }
     glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
